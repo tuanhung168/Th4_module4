2700e278f41df70757710c97bfd7082b80e56aac
tuanhung168 <96073469+tuanhung168@users.noreply.github.com>
2023-08-02 10:33:31 +0700

    Add files via upload

---
+++ MD4-Session10-th4-main/src/main/java/ra/th4/HelloServlet.java
@@ -0,0 +1,32 @@
+package ra.th4;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/translate")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Map<String, String> dictionary = new HashMap<>();
+        dictionary.put("hello", "Xin chào");
+        dictionary.put("how", "Thế nào");
+        dictionary.put("book", "Quyển vở");
+        dictionary.put("computer", "Máy tính");
+        String search = request.getParameter("txtSearch");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+
+        String result = dictionary.get(search);
+        if (result != null) {
+            writer.println("Word: " + search);
+            writer.println("Result: " + result);
+        } else {
+            writer.println("Not found");
+        }
+        writer.println("</html>");
+    }
+}
